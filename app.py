8b56f9a6c552f60bf4c96fe66f26312f4d19d870
Grava <zx357951123@gmail>
2018-05-31 12:15:55 +0800

    app.py

---
+++ app.py
@@ -0,0 +1,49 @@
+# encoding: utf-8
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+app = Flask(__name__)
+
+# 填入你的 message api 資訊
+line_bot_api = LineBotApi('YOUR CHANNEL ACCESS TOKEN')
+handler = WebhookHandler('YOUR CHANNEL SECRET')
+
+# 設定你接收訊息的網址，如 https://YOURAPP.herokuapp.com/callback
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    print("Request body: " + body, "Signature: " + signature)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    print("Handle: reply_token: " + event.reply_token + ", message: " + event.message.text)
+    content = "{}: {}".format(event.source.user_id, event.message.text)
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=content))
+
+import os
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=os.environ['PORT'])
