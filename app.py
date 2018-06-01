a529fe13f8e200c273a645b7b7bdab08c560c5f8
Grava <zx357951123@gmail>
2018-06-01 10:13:03 +0800

    send image message

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
 )
 
 app = Flask(__name__)
@@ -16,7 +16,7 @@ app = Flask(__name__)
 # 填入你的 message api 資訊
 Token = "hR+zZ1KpsaRQYcF9CIvvsc4rVvZ1tHaQgjz1JlBOIvLLtMW7n6yaSHqhksoJtqWKw7iOBSoO6bKpIxJbk/VPAW/+ROcRTU5L5kYEiX8WhJhSWqU1YFhQkK0knhTkVSd3LlHDZ2C/LMi6GW8GU7U2PwdB04t89/1O/w1cDnyilFU="
 channel = "69df63f9a6f70870fc131c5d6045d4a9"
-userID = "Ua1f0d71da0aa5d12d0132aba8cff150f"
+# userID = "Ua1f0d71da0aa5d12d0132aba8cff150f"
 
 line_bot_api = LineBotApi(Token)
 handler = WebhookHandler(channel)
@@ -43,11 +43,27 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     print("Handle: reply_token: " + event.reply_token + ", message: " + event.message.text)
-    content = "{}: {}".format(event.source.user_id, event.message.text)
+    # content = "{}: {}".format(event.source.user_id, event.message.text)
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=content))
+        returnContent(event.message.text))
+
 
 import os
+def returnContent(U_Receive,Mode=""):
+    C = TextSendMessage(text=U_Receive)
+    if Mode is "img" or U_Receive == "抽":
+        img = RandomPic()
+        C = ImageSendMessage(
+            type='image',
+            original_content_url=img,  # Pic Url
+            preview_image_url=img  # Preview Pic
+        )
+    return C
+
+def RandomPic():
+    img = "https://m1.ablwang.com/uploadfile/2017/0901/20170901042508280.jpg"
+    return img
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=os.environ['PORT'])
